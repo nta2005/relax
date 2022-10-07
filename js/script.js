5264b81d7f2b54fa52f8517505f280e849a88811
nta2005 <ntapro1412@gmail.com>
2022-10-07 17:44:30 +0700

    update time delay

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
   $(".content a").fancybox();
   TweenMax.staggerFrom(
     $(".content a"), // phần tử được chọn
-    0.2, // thời gian chuyển động
+    0.1, // thời gian chuyển động
     { top: 100, opacity: 0 },
-    0.2 // thời gian cách nhau giữa mỗi hiệu ứng
+    0.1 // thời gian cách nhau giữa mỗi hiệu ứng
   );
 });
